d520bc2c26614cc2f64b6cf1609aaf8d3659ee41
Samuel Getachew <falconroman36@gmail.com>
2024-04-04 13:01:50 +0300

    commited

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -10,24 +10,28 @@ const Rating = () => {
   return (
     <div className="flex">
       {[...Array(5)].map((_, index) => (
-        <svg
-          key={index}
-          xmlns="http://www.w3.org/2000/svg"
-          className={`h-6 w-6 cursor-pointer ${
-            index < rating ? "text-yellow-500" : "text-gray-300"
-          }`}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          onClick={() => handleStarClick(index)}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 14l-4 2.5 1-4-3-2.5h4l2-4 2 4h4l-3 2.5 1 4z"
-          />
-        </svg>
+        <div key={index} className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-10 w-10 cursor-pointer ${
+              index < rating ? "text-yellow-500" : "text-gray-700"
+            }`}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            onClick={() => handleStarClick(index)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 14l-4 2.5 1-4-3-2.5h4l2-4 2 4h4l-3 2.5 1 4z"
+            />
+          </svg>
+          <span className="ml-1 text-xl text-green-600 font-medium">
+            {index + 1}
+          </span>
+        </div>
       ))}
     </div>
   );
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -6,8 +6,28 @@ import Helmet from "./../components/Helmet/Helmet";
 import Rating from "../components/Rating/Rating";
 import { motion } from "framer-motion";
 import ProductsList from "../components/UI/ProductsList";
+import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../Redux/slices/cartslice";
+import { toast } from "react-toastify";
 
 const ProductDetail = () => {
+  const reviewUser = useRef("");
+  const reviewMessage = useRef("");
+  const dispatch = useDispatch();
+  const addToCart = () => {
+    dispatch(
+      cartActions.addItem({
+        id,
+        productName: title,
+        Price: Price,
+        image: ProductsImage,
+      })
+    );
+    toast.success("product added successfully");
+  };
+
+  const [rev, setRev] = useState(false);
   const { id } = useParams();
   const parsedId = parseInt(id);
   const product = Products.find((item) => item.id === parsedId);
@@ -27,13 +47,20 @@ const ProductDetail = () => {
   const { ProductsImage, title, shortDesc, Price, reviews, type } = product;
   const relatedProducts = Products.filter((item) => item.type === type);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const reviewUserName = reviewUser.current.value;
+    const reviewUserMessage = reviewMessage.current.value;
+  };
+
   return (
     <>
       <Helmet>
         <section>
           <CommonSection title={"Detail"} />
-          <div className="flex flex-wrap justify-center gap-10 items-center mt-40">
-            <div className="lg:w-[40%] w-full">
+          <div className="flex flex-wrap justify-center items-center mt-10">
+            <div className="">
               <img src={ProductsImage} alt="" />
             </div>
             {/*  */}
@@ -43,10 +70,11 @@ const ProductDetail = () => {
                 <Rating />
               </div>
               <div className="text-md text-gray-500">{shortDesc}</div>
-              <div className="text-2xl font-medium pb-20">{Price}</div>
+              <div className="text-2xl font-medium pb-5">{Price}</div>
               <motion.button
                 whileTap={{ scale: 1.1 }}
                 className="px-10 py-2 font-medium text-white bg-green-800 rounded-md"
+                onClick={addToCart}
               >
                 Add to Cart
               </motion.button>
@@ -57,44 +85,56 @@ const ProductDetail = () => {
         <section className="px-20">
           <div className="flex flex-wrap justify-evenly mt-10">
             <div className="">
-              <h2 className="text-2xl font-medium text-green-900 mb-5">
-                Reviews
+              <h2
+                className="text-2xl font-medium text-green-900 mb-5 cursor-pointer"
+                onClick={() => setRev(true)}
+              >
+                Reviews({reviews.length})
               </h2>
-              <ul className="pb-20">
-                {reviews.map((item, index) => (
-                  <>
-                    <li key={index}>
-                      <h3 className="font-medium text-lg">Sami Getu</h3>
-                      <p className="text-green-600 font-medium">
-                        {item.rating}(Rating)
-                      </p>
-                      <p className="text-gray-500 font-medium">{item.text}</p>
-                    </li>
-                  </>
-                ))}
-              </ul>
+              {rev && (
+                <>
+                  <ul className="pb-20">
+                    {reviews.map((item, index) => (
+                      <>
+                        <li key={index}>
+                          <h3 className="font-medium text-lg">Sami Getu</h3>
+                          <p className="text-green-600 font-medium">
+                            {item.rating}(Rating)
+                          </p>
+                          <p className="text-gray-500 font-medium">
+                            {item.text}
+                          </p>
+                        </li>
+                      </>
+                    ))}
+                  </ul>
+                </>
+              )}
+
               <div className="flex justify-center">
-                <form action="" className="space-y-5">
-                  <h4 className="text-3xl font-medium">
+                <form action="" onSubmit={submitHandler} className="space-y-5">
+                  <h4 className="text-3xl font-medium text-neutral-800">
                     Share us your Experience
                   </h4>
                   <div>
                     <input
-                      className="xl:px-[30rem] lg:px-[20rem] md:px-[10rem] pl-10 border-2 py-2 rounded-md font-medium  focus:outline-none text-start"
+                      className="xl:px-[30rem] lg:px-[20rem] md:px-[10rem] pl-10 border-[1px] py-2 rounded-md border-green-600 font-medium  focus:outline-none text-start"
                       type="text"
                       placeholder="Enter name"
+                      ref={reviewUser}
                     />
                   </div>
                   <div>
                     <span>
-                      <Rating />
+                      <Rating className="text-2xl" />
                     </span>
                   </div>
                   <div>
                     <textarea
-                      className="xl:px-[30rem] lg:px-[20rem] md:px-[10rem] pl-10 border-2 py-10 rounded-md font-medium focus:outline-none text-start"
+                      className="xl:px-[30rem] lg:px-[20rem] md:px-[10rem] pl-10 border-[1px] py-10 rounded-md border-green-600 font-medium focus:outline-none text-start"
                       type="text"
                       placeholder="Message..."
+                      ref={reviewMessage}
                     />
                   </div>
                   <input
@@ -109,7 +149,7 @@ const ProductDetail = () => {
 
         <section className="mt-[5rem]">
           <div className="flex flex-col justify-center">
-            <h6 className="text-5xl text-center font-medium">
+            <h6 className="text-5xl text-neutral-800 text-center font-medium">
               You Also Might Like
             </h6>
             <div>
